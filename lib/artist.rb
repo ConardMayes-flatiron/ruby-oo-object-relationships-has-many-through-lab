ab1f722cbf6b0505f36ee3b6bbc09928f66a5381
ConardMayes-flatiron <ConradAsatoMayes+flatironschool@gmail.com>
2020-12-15 23:26:39 -0600

    commit tree f4a214a17821dad5882b9253ceb2dea7ae5e9c42

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,1 +1,38 @@
+require "pry"
 
+class Artist
+    attr_accessor :name, :genre, :song
+
+    @@all =[]
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_song(name, genre)
+        Song.new(name, self, genre)
+    end
+
+    def songs
+        Song.all.select do |track|
+            track.artist == self
+        end
+    end
+
+    def genres
+        # binding.pry
+        self.songs.map do |track|
+            track.genre
+        end
+    end
+        
+
+
+
+
+end
--- lib/genre.rb
+++ lib/genre.rb
@@ -1,1 +1,37 @@
 
+class Genre
+    attr_accessor :name
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def songs
+        Song.all.select do |track|
+            track.genre == self
+        end
+    end
+
+    def artists
+        self.songs.map do |track|
+            track.artist
+        end
+    end
+
+
+
+
+
+
+
+
+
+
+end
